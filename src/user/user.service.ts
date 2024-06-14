a8958d2009cc9ddec42cb27585be8df8b39d8be3
Nidhiy05 <navanidhiyachuthan05@gmail.com>
2024-06-14 17:17:16 +0400

    user.service.ts

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -4,6 +4,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
@@ -12,14 +13,17 @@ export class UserService {
     private usersRepository: Repository<User>,
   ) {}
 
-  async findOne(email: string): Promise<User | undefined> {
-    return this.usersRepository.findOne({ where: { email } });
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
+    const user = this.usersRepository.create({ ...createUserDto, password: hashedPassword });
+    return this.usersRepository.save(user);
   }
 
-  async create(createUserDto: CreateUserDto): Promise<User> {
-    const user = new User();
-    user.email = createUserDto.email;
-    user.password = createUserDto.password; // hashed password
-    return this.usersRepository.save(user);
+  async findAll(): Promise<User[]> {
+    return this.usersRepository.find();
+  }
+
+  async findByEmail(email: string): Promise<User | undefined> {
+    return this.usersRepository.findOne({ where: { email } });
   }
 }
